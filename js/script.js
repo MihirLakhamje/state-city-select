b74f5a44ce5ee7f78be21712e989883c72641409
MihirLakhamje <lakhamjemihir@gmail.com>
2022-09-29 01:25:18 +0530

    update

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 // Initialize variables
 let _state, _city;
 let stateOpt = document.querySelector(".stateOpt")
-let cityOpt = document.querySelector(".cityOpt")
+let cityOpt = document.getElementsByClassName(".cityOpt")
 let SselecT = document.getElementById("state");
 let CselecT = document.getElementById("city");
 
 // JSON
-const Select = '{"state":[{"stateName":"Maharastra","city":["Mumbai","Beed","Kaij"]},{"stateName":"Rajasthan","city":["Bambir","rajasthan state 1","rajasthan state 2"]}]}';
+const Select = '{"state":[{"stateName":"Maharastra","city":["Mumbai","Beed","Kaij"]},{"stateName":"Rajasthan","city":["Jodhpur","Jaipur","Udaipur"]}]}';
 
 // Convert into object
 const values = JSON.parse(Select);
@@ -25,16 +25,16 @@ values["state"].forEach((fetchstate, stateIndex) => {
 
 // on every change 
 SselecT.addEventListener("change",function(e){
-
+    
     // Fetch state
     values.state.forEach((ele, i)=>{
-
+        
         // whether selected option match or not
         if(this[this.selectedIndex].text === ele.stateName){
 
             // Fetch city and display in option tag
             ele.city.forEach((fetchcity)=>{
-                _city = `<option class="stateOpt" value=${fetchcity}>${fetchcity}</option>`;
+                _city = `<option class="cityOpt" value=${fetchcity}>${fetchcity}</option>`;
                 CselecT.innerHTML += _city;
             });
         }
