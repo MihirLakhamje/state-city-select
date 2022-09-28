8a9d1e5116f24fe5c73d8a36e11a139bc6cd7474
MihirLakhamje <lakhamjemihir@gmail.com>
2022-09-28 19:01:35 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,26 +3,22 @@ let stateOpt = document.querySelector(".stateOpt")
 let cityOpt = document.querySelector(".cityOpt")
 let SselecT = document.getElementById("state");
 let CselecT = document.getElementById("city");
-const Select = '{"state":[{"stateName":"Maharastra","city":["Mumbai","Beed","Kaij"]},{"stateName":"Rajasthan","city":["Bambir","Delhi","Chennai"]}]}';
-const values = JSON.parse(Select);
 
-console.log(values);
+const Select = '{"state":[{"stateName":"Maharastra","city":["Mumbai","Beed","Kaij"]},{"stateName":"Rajasthan","city":["Bambir","rajasthan state 1","rajasthan state 2"]}]}';
+const values = JSON.parse(Select);
 
 values["state"].forEach((fetchstate, stateIndex) => {
     console.log(fetchstate, stateIndex);
     
     _state = `<option class="stateOpt" value=${fetchstate.stateName}>${fetchstate.stateName}</option>`;
     SselecT.innerHTML += _state;
-    
-   
-    SselecT.addEventListener("change",function(e){
-        console.log(this[this.selectedIndex].text);
+    if(this[this.selectedIndex]){
+        console.log(fetchstate.city)
         
-        if(this[this.selectedIndex]){
-            console.log(fetchstate.city)
-            return true
-        }
-    });
-
+    }
 });
 
+SselecT.addEventListener("change",function(e){
+    console.log(this[this.selectedIndex].text);
+    
+});
